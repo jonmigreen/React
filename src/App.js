751601df48800b1bd7aee10d4eccde496c67d184
jonmigreen <86339058+jonmigreen@users.noreply.github.com>
2022-02-25 22:43:04 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <body>
+      <div id="main-header">
+        <div class="container">
+          <h1 id="logo"> JG </h1>
+          <nav id="navbar">
+            <ul>
+              <li>
+                <a href="https://google.com">Work</a>
+              </li>
+              <li>
+                <a href="https://google.com">About</a>
+              </li>
+              <li>
+                <a href="https://google.com">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </body>
+  );
+}
